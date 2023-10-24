44fc56824222fe04bdbd893cbd9e111b7d5a91de
jasmi <jasmi@MSI>
2023-10-24 16:58:31 +0100

    Add project files.

---
+++ Project1/Block.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Block
+    {
+        public Block(int width, int height, Color colour)
+        {
+            Block block1 = new Block(20, 30, Color.BlueViolet);
+        }
+    }
+}
---
+++ Project1/Game1.cs
@@ -0,0 +1,66 @@
+﻿using ImGuiNET;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.ImGui;
+using System.Collections.Generic;
+
+namespace Project1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _pacman;
+        private Vector2 _pacmanposition;
+        private Vector2 _blockposition;
+        private ShapeBatcher _shapeBatcher;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //_pacman = Content.Load<Texture2D>("pacman_PNG25");
+            //_pacmanposition = Vector2.Zero;
+            // TODO: use this.Content to load your game content here
+            _blockposition = new Vector2(300, 400);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            //_spriteBatch.Draw(_pacman, _pacmanposition, Color.White);
+            _spriteBatch.End();
+
+            _shapeBatcher.DrawRectangle(50, 30, Color.White);
+            base.Draw(gameTime);
+        }
+    }
+}
