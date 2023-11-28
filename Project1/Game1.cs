37f1617dd7d723448009d21e1f526f6ba8cd72a9
Jasmine <jasmine.jolly-2021@hull.ac.uk>
2023-11-28 14:52:37 +0000

    hfgworihg

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -19,34 +19,35 @@ namespace Project1
         Texture2D coverUp;
         public Texture2D PacmanTexture, WallTexture, PinkTexture, RedTexture, BlueTexture, OrangeTexture;
         public Rectangle Pacman, Red, Pink, Blue, Orange;
-        private Rectangle Wall1;
-        private Rectangle Wall2;
-        private Rectangle Wall3;
-        private Rectangle Wall4;
-        private Rectangle Wall5;
-        private Rectangle Wall6;
-        private Rectangle Wall7;
-        private Rectangle Wall8;
-        private Rectangle Wall9;
-        private Rectangle Wall10;
-        private Rectangle Wall11;
-        private Rectangle Wall12;
-        private Rectangle Wall13;
-        private Rectangle Wall14;
-        private Rectangle Wall15;
-        private Rectangle Wall16;
-        private Rectangle Wall17;
-        private Rectangle Wall18;
-        private Rectangle Wall19;
-        private Rectangle Wall20;
-        private Rectangle Wall21;
-        private Rectangle Wall22;
-        private Rectangle Wall23;
-        private Rectangle Wall24;
+        private List<Rectangle> _walls;
+        //private Rectangle Wall1;
+        //private Rectangle Wall2;
+        //private Rectangle Wall3;
+        //private Rectangle Wall4;
+        //private Rectangle Wall5;
+        //private Rectangle Wall6;
+        //private Rectangle Wall7;
+        //private Rectangle Wall8;
+        //private Rectangle Wall9;
+        //private Rectangle Wall10;
+        //private Rectangle Wall11;
+        //private Rectangle Wall12;
+        //private Rectangle Wall13;
+        //private Rectangle Wall14;
+        //private Rectangle Wall15;
+        //private Rectangle Wall16;
+        //private Rectangle Wall17;
+        //private Rectangle Wall18;
+        //private Rectangle Wall19;
+        //private Rectangle Wall20;
+        //private Rectangle Wall21;
+        //private Rectangle Wall22;
+        //private Rectangle Wall23;
+        //private Rectangle Wall24;
         public Vector2 Position, Direction, Acceleration;
         public float Speed;
-        private GameTime gameTime;
-        
+        private readonly List<Ghost> _ghosts = new();
+
 
         //private ShapeBatcher _shapeBatcher;
 
@@ -83,35 +84,51 @@ namespace Project1
             Red = new Rectangle(760, 0, 30, 30);
             Blue = new Rectangle(760, 0, 30, 30);
             Orange = new Rectangle(760, 0, 30, 30);
-            Wall1 = new Rectangle(0, 80, 80, 40);
-            Wall2 = new Rectangle(0, 400, 80, 80);
-            Wall3 = new Rectangle(164, 0, 40, 112);
-            Wall4 = new Rectangle(164, 112, 144, 40);
-            Wall5 = new Rectangle(352, 0, 40, 280);
-            Wall6 = new Rectangle(598, 0, 40, 160);
-            Wall7 = new Rectangle(486, 120, 112, 40);
-            Wall8 = new Rectangle(176, 400, 20, 80);
-            Wall9 = new Rectangle(196, 400, 80, 20);
-            Wall10 = new Rectangle(256, 320, 20, 80);
-            Wall11 = new Rectangle(276, 320, 60, 20);
-            Wall12 = new Rectangle(296, 450, 80, 30);
-            //Wall13 = new Rectangle(296, 420, 40, 20);
-            Wall14 = new Rectangle(416, 400, 40, 80);
-            Wall15 = new Rectangle(456, 440, 136, 40);
-            Wall16 = new Rectangle(552, 400, 40, 80);
-            Wall17 = new Rectangle(477, 320, 64, 40);
-            Wall18 = new Rectangle(656, 304, 144, 40);
-            Wall19 = new Rectangle(760, 344, 40, 140);
-            Wall20 = new Rectangle(88, 184, 40, 148);
-            Wall21 = new Rectangle(192, 208, 96, 60);
-            Wall22 = new Rectangle(432, 36, 120, 40);
-            Wall23 = new Rectangle(432, 224, 240, 40);
-            Wall24 = new Rectangle(648, 384, 60, 52);
+            _walls = new List<Rectangle>();
 
-           
+            _walls.Add(new Rectangle(0, 80, 80, 40));
+            _walls.Add(new Rectangle(0, 400, 80, 80));
+            _walls.Add(new Rectangle(164, 0, 40, 112));
+            _walls.Add(new Rectangle(164, 112, 144, 40));
+            _walls.Add(new Rectangle(352, 0, 40, 280));
+            _walls.Add(new Rectangle(598, 0, 40, 160));
+            _walls.Add(new Rectangle(486, 120, 112, 40));
+            _walls.Add(new Rectangle(176, 400, 20, 80));
+            _walls.Add(new Rectangle(196, 400, 80, 20));
+            _walls.Add(new Rectangle(256, 320, 20, 80));
+            _walls.Add(new Rectangle(276, 320, 60, 20));
+            _walls.Add(new Rectangle(296, 450, 80, 30));
+            //Wall13 = new Rectangle(296, 420, 40, 20);
+            _walls.Add(new Rectangle(416, 400, 40, 80));
+            _walls.Add(new Rectangle(456, 440, 136, 40));
+            _walls.Add(new Rectangle(552, 400, 40, 80));
+            _walls.Add(new Rectangle(477, 320, 64, 40));
+            _walls.Add(new Rectangle(656, 304, 144, 40));
+            _walls.Add(new Rectangle(760, 344, 40, 140));
+            _walls.Add(new Rectangle(88, 184, 40, 148));
+            _walls.Add(new Rectangle(192, 208, 96, 60));
+            _walls.Add(new Rectangle(432, 36, 120, 40));
+            _walls.Add(new Rectangle(432, 224, 240, 40));
+            _walls.Add(new Rectangle(648, 384, 60, 52));
+
+            
+
+                var ghosttexture = Content.Load<Texture2D>("pinkghost");
+
+                var ai = new Around();
+                ai.AddPoint(new(100, 100));
+                ai.AddPoint(new(400, 100));
+                ai.AddPoint(new(400, 400));
+                ai.AddPoint(new(100, 400));
+
+                _ghosts.Add(new(ghosttexture, new(750, 50))
+                {
+                    MoveAI = ai
+                });
+            
         }
 
-       
+
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
@@ -132,22 +149,22 @@ namespace Project1
 
             if (Keyboard.GetState().IsKeyDown(Keys.W))
             {
-                Pacman.Y = Pacman.Y - 5;
+                _pacmanposition.Y = _pacmanposition.Y - 5;
             }
 
             if (Keyboard.GetState().IsKeyDown(Keys.S))
             {
-                Pacman.Y = Pacman.Y + 5;
+                _pacmanposition.Y = _pacmanposition.Y + 5;
             }
 
             if (Keyboard.GetState().IsKeyDown(Keys.A))
             {
-                Pacman.X = Pacman.X - 5;
+                _pacmanposition.X = _pacmanposition.X - 5;
             }
 
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
-                Pacman.X = Pacman.X + 5;
+                _pacmanposition.X = _pacmanposition.X + 5;
             }
 
             Acceleration *= Speed;
@@ -159,103 +176,92 @@ namespace Project1
             Position += Direction;
 
             //if (Wall.Intersects(Pacman))
-            //{
-            //    //Intersection is true. Now add the bouncing logic
+            
+                //Intersection is true. Now add the bouncing logic
 
-            List<Rectangle> walls = new List<Rectangle>();
 
-            walls.Add(Wall1);
-            walls.Add(Wall2);
-            walls.Add(Wall3);
-            walls.Add(Wall4);
-            walls.Add(Wall5);
-            walls.Add(Wall6);
-            walls.Add(Wall7);
-            walls.Add(Wall8);
-            walls.Add(Wall9);
-            walls.Add(Wall10);
-            walls.Add(Wall11);
-            walls.Add(Wall12);
-            walls.Add(Wall13);
-            walls.Add(Wall14);
-            walls.Add(Wall15);
-            walls.Add(Wall16);
-            walls.Add(Wall17);
-            walls.Add(Wall18);
-            walls.Add(Wall19);
-            walls.Add(Wall20);
-            walls.Add(Wall21);
-            walls.Add(Wall22);
-            walls.Add(Wall23);
-            walls.Add(Wall24);
 
-            foreach (Rectangle rectangle in walls)
-            {
-                if (_pacmanposition.X < rectangle.Right && _pacmanposition.Y > (rectangle.Top) && _pacmanposition.Y < rectangle.Bottom)
+                foreach (Rectangle rectangle in _walls)
                 {
-                    _pacmanposition.X = _pacmanposition.X + 5;
+                    if (_pacmanposition.X < rectangle.Right && _pacmanposition.Y > (rectangle.Top) && _pacmanposition.Y < rectangle.Bottom)
+                    {
+                        _pacmanposition.X = _pacmanposition.X + 5;
+                    }
+
+                    if (_pacmanposition.X > rectangle.Left && _pacmanposition.Y > (rectangle.Top) && _pacmanposition.Y < rectangle.Bottom)
+                    {
+                        _pacmanposition.X = _pacmanposition.X - 5;
+                    }
+
+                    if (_pacmanposition.Y == rectangle.Bottom && _pacmanposition.X < rectangle.Right && _pacmanposition.X > -5)
+                    {
+                        _pacmanposition.Y = _pacmanposition.Y - 5;
+                    }
+
+                    if (_pacmanposition.Y == rectangle.Top && _pacmanposition.X < rectangle.Right && _pacmanposition.X > -5)
+                    {
+                        _pacmanposition.Y = _pacmanposition.Y + 5;
+                    }
+                }
+                base.Update(gameTime);
+
+                foreach (var ghost in _ghosts)
+                {
+                    ghost.Update();
                 }
 
-                if (_pacmanposition.X > rectangle.Left && _pacmanposition.Y > (rectangle.Top) && _pacmanposition.Y < rectangle.Bottom)
-                {
-                    _pacmanposition.X = _pacmanposition.X - 5;
-                }
-
-                if (_pacmanposition.Y == rectangle.Bottom && _pacmanposition.X < rectangle.Right && _pacmanposition.X > -5)
-                {
-                    _pacmanposition.Y = _pacmanposition.Y - 5;
-                }
-
-                if (_pacmanposition.Y == rectangle.Top && _pacmanposition.X < rectangle.Right && _pacmanposition.X > -5)
-                {
-                    _pacmanposition.Y = _pacmanposition.Y + 5;
-                }
-            }
-            base.Update(gameTime);
 
 
-        
+            
+        }
 
-    }
-
-        protected override void Draw(GameTime gametime)
+        protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            //// TODO: Add your drawing code here
             _spriteBatch.Begin();
             _spriteBatch.Draw(_pacman, _pacmanposition, Color.White);
-            _spriteBatch.Draw(WallTexture, Wall1, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall2, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall3, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall4, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall5, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall6, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall7, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall8, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall9, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall10, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall11, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall12, Color.BlueViolet);
-            //_spriteBatch.Draw(WallTexture, Wall13, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall14, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall15, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall16, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall17, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall18, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall19, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall20, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall21, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall22, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall23, Color.BlueViolet);
-            _spriteBatch.Draw(WallTexture, Wall24, Color.BlueViolet);
-            _spriteBatch.Draw(PacmanTexture, Pacman, Color.White);
-            //_spriteBatch.Draw(PinkTexture, Pink, Color.White);
-            //_spriteBatch.Draw(RedTexture, Red, Color.White);
-            //_spriteBatch.Draw(BlueTexture, Blue, Color.White);
-            //_spriteBatch.Draw(OrangeTexture, Orange, Color.White);
-            //_spriteBatch.Draw(coverUp, new Rectangle(50, 50, 20, 15), Color.Blue);
-            //_spriteBatch.Draw(coverUp, new Rectangle(0, GameData.TileSize.Y * GameData.TileCount.Y + GameData.LevelOffset.Y, GameData.TileCount.X * GameData.TileSize.X, 50), Color.Blue);
+            //_spriteBatch.Draw(WallTexture, Wall1, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall2, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall3, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall4, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall5, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall6, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall7, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall8, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall9, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall10, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall11, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall12, Color.BlueViolet);
+            ////_spriteBatch.Draw(WallTexture, Wall13, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall14, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall15, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall16, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall17, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall18, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall19, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall20, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall21, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall22, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall23, Color.BlueViolet);
+            //_spriteBatch.Draw(WallTexture, Wall24, Color.BlueViolet);
+            //_spriteBatch.Draw(PacmanTexture, Pacman, Color.White);
+
+            foreach (Rectangle wall in _walls)
+            {
+                _spriteBatch.Draw(WallTexture, wall, Color.BlueViolet);
+            }
+
+            foreach (var ghost in _ghosts)
+            {
+                ghost.Draw(_spriteBatch);
+            }
+            ////_spriteBatch.Draw(PinkTexture, Pink, Color.White);
+            ////_spriteBatch.Draw(RedTexture, Red, Color.White);
+            ////_spriteBatch.Draw(BlueTexture, Blue, Color.White);
+            ////_spriteBatch.Draw(OrangeTexture, Orange, Color.White);
+
             _spriteBatch.End();
 
             //_shapeBatcher.DrawRectangle(50, 30, Color.White);
--- Project1/Ghost.cs
+++ Project1/Ghost.cs
@@ -1,23 +1,35 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project1
 {
-    public class Ghost : Game1
+    public class Ghost
     {
         public Movement MoveAI { get; set; }
+        public float Speed;
+        public Vector2 Position;
+        public Texture2D Texture;
+
         public Ghost(Texture2D tex, Vector2 pos)
         {
+            Texture = tex;
+            Position = pos;
             Speed = 250;
         }
         public void Update()
         {
             MoveAI.Move(this);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle rect = new Rectangle((int)Position.X, (int)Position.Y, 30, 30);
+            spriteBatch.Draw(Texture, rect, Color.White);
         }
     }
 }
