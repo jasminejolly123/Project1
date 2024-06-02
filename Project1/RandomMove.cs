a1c03588e7f7903e04abd2a65944f58e454d590d
Jasmine <jasmine.jolly-2021@hull.ac.uk>
2024-06-02 16:02:33 +0100

    state machine for orange

--- Project1/RandomMove.cs
+++ Project1/RandomMove.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
@@ -14,54 +15,55 @@ namespace Project1
     {
         public Pacman Target { get; set; }
         private System.Random rng = new System.Random();
+        private Orange currentState = Orange.RandomMove;
+
+        public enum Orange
+        {
+            Still,
+            Chase,
+            RandomMove
+        }
+
         public override void Move(Sprite ghost)
         {
-            Walls();
+            switch (currentState)
+            {
+                case Orange.Still:
+                    UpdateStill();
+                    break;
+                case Orange.Chase:
+                    UpdateChase(ghost);
+                    break;
+                case Orange.RandomMove:
+                    UpdateRandomMove(ghost);
+                    break;
+
+                default: break;
+            }
+        }
+
+
+        public void UpdateRandomMove(Sprite ghost)
+        {
             Vector2 OldPosition = ghost.Position;
-            
-            if (Target is null) return;
+            Walls();
 
-            Main();
+            if (Target.Position == ghost.Position)
+            {
+                currentState = Orange.Still;
+            }
 
-            //float dis1 = (ghost.Position.X - Target.Position.X) * (ghost.Position.X - Target.Position.X);
-            //float dis2 = (ghost.Position.Y - Target.Position.Y) * (ghost.Position.Y - Target.Position.Y);
-            //float dis3 = dis1 + dis2;
-            //float dis4 = (float)Math.Sqrt(dis3);
-
-            //float dis5 = (ghost.Position.X - pos.X) * (ghost.Position.X - pos.X);
-            //float dis6 = (ghost.Position.Y - pos.Y) * (ghost.Position.Y - pos.Y);
-            //float dis7 = dis5 + dis6;
-            //float dis8 = (float)Math.Sqrt(dis7);
-            //var dir1 = Target.Position - ghost.Position;
-            //var dir2 = pos - ghost.Position;
-
-            //if (dir1.Length() > 4 && dis8 < dis4)
-            //{
-            //    dir1.Normalize();
-            //    ghost.Position += dir2 * ghost.Speed * Globals.TotalSeconds;
-            //}
-            //else
-            //{
-            //    dir2.Normalize();
-            //    ghost.Position += dir1 * ghost.Speed * Globals.TotalSeconds;
-            //}
-
-            //int pos = rng.Next(0, 400);
-
-            Vector2 pos = new Vector2(0, 0);
-
-            while (pos != ghost.Position)
+            while (Target.Position != ghost.Position)
             {
 
                 int num = rng.Next(0, 400);
                 int num2 = rng.Next(0, 800);
-                pos = new Vector2(num2, num);
-                var dir = pos - ghost.Position;
+                Target.Position = new Vector2(num2, num);
+                var dir = Target.Position - ghost.Position;
 
                 if (dir.Length() > 4)
                 {
-                    dir.Normalize();
-                    ghost.Position += dir * ghost.Speed * Globals.TotalSeconds;
+                    currentState = Orange.Chase;
                 }
 
                 foreach (Rectangle rectangle in _walls)
@@ -73,6 +75,88 @@ namespace Project1
                 }
             }
         }
+
+
+
+        public void UpdateChase(Sprite ghost)
+        {
+            var dir = Target.Position - ghost.Position;
+            dir.Normalize();
+            ghost.Position += dir * ghost.Speed * Globals.TotalSeconds;
+        }
+
+
+
+        public void UpdateStill()
+        {
+
+        }
+
     }
 }
+
+
+
+//        public override void Move(Sprite ghost)
+//        {
+            
+//            Walls();
+//            Vector2 OldPosition = ghost.Position;
+            
+//            if (Target is null) return;
+
+//            Main();
+
+//            //float dis1 = (ghost.Position.X - Target.Position.X) * (ghost.Position.X - Target.Position.X);
+//            //float dis2 = (ghost.Position.Y - Target.Position.Y) * (ghost.Position.Y - Target.Position.Y);
+//            //float dis3 = dis1 + dis2;
+//            //float dis4 = (float)Math.Sqrt(dis3);
+
+//            //float dis5 = (ghost.Position.X - pos.X) * (ghost.Position.X - pos.X);
+//            //float dis6 = (ghost.Position.Y - pos.Y) * (ghost.Position.Y - pos.Y);
+//            //float dis7 = dis5 + dis6;
+//            //float dis8 = (float)Math.Sqrt(dis7);
+//            //var dir1 = Target.Position - ghost.Position;
+//            //var dir2 = pos - ghost.Position;
+
+//            //if (dir1.Length() > 4 && dis8 < dis4)
+//            //{
+//            //    dir1.Normalize();
+//            //    ghost.Position += dir2 * ghost.Speed * Globals.TotalSeconds;
+//            //}
+//            //else
+//            //{
+//            //    dir2.Normalize();
+//            //    ghost.Position += dir1 * ghost.Speed * Globals.TotalSeconds;
+//            //}
+
+//            //int pos = rng.Next(0, 400);
+
+//            Vector2 pos = new Vector2(0, 0);
+
+//            while (pos != ghost.Position)
+//            {
+
+//                int num = rng.Next(0, 400);
+//                int num2 = rng.Next(0, 800);
+//                pos = new Vector2(num2, num);
+//                var dir = pos - ghost.Position;
+
+//                if (dir.Length() > 4)
+//                {
+//                    dir.Normalize();
+//                    ghost.Position += dir * ghost.Speed * Globals.TotalSeconds;
+//                }
+
+//                foreach (Rectangle rectangle in _walls)
+//                {
+//                    if (rectangle.Contains(ghost.Position))
+//                    {
+//                        ghost.Position = OldPosition;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
